c6b2b7f5f843d441e342ac16453cb8f9e552f0d0
Octobarian <rileybcg@gmail.com>
2024-03-31 15:19:39 -0600

    Remove Auth for now

--- app/middleware/checkSecret.js
+++ app/middleware/checkSecret.js
@@ -1,18 +1,18 @@
 const CryptoJS = require("crypto-js");
 
 function checkSecret(req, res, next) {
-  const encryptedClientSecret = req.headers['cust-server-secret'];
-  if (!encryptedClientSecret) {
-    return res.status(403).send({ message: 'Server secret is missing i see: '+JSON.stringify(req.headers) });
-  }
+//   const encryptedClientSecret = req.headers['cust-server-secret'];
+//   if (!encryptedClientSecret) {
+//     return res.status(403).send({ message: 'Server secret is missing i see: '+JSON.stringify(req.headers) });
+//   }
 
-  // Decrypt the client secret
-  const bytes = CryptoJS.AES.decrypt(encryptedClientSecret, process.env.SERVER_ENCRYPTION_KEY);
-  const clientSecret = bytes.toString(CryptoJS.enc.Utf8);
+//   // Decrypt the client secret
+//   const bytes = CryptoJS.AES.decrypt(encryptedClientSecret, process.env.SERVER_ENCRYPTION_KEY);
+//   const clientSecret = bytes.toString(CryptoJS.enc.Utf8);
 
-  if (clientSecret !== process.env.SERVER_SECRET_KEY) {
-    return res.status(403).send({ message: 'Invalid server secret' });
-  }
+//   if (clientSecret !== process.env.SERVER_SECRET_KEY) {
+//     return res.status(403).send({ message: 'Invalid server secret' });
+//   }
   next();
 }
 module.exports = checkSecret;
