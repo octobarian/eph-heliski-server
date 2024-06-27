4905df52b11760502e5c84c6746db1c43aa5f89c
Octobarian <rileybcg@gmail.com>
2024-06-26 23:40:58 -0600

    set refresh on migrations

--- app/migrations-old/add_sortingIndex_to_trips.sql
+++ app/migrations-old/add_sortingIndex_to_trips.sql
@@ -1,9 +1,9 @@
--- 20240622_add_sortingIndex_to_trips.sql
+-- -- 20240622_add_sortingIndex_to_trips.sql
 
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='trips' AND column_name='sortingIndex') THEN
-        ALTER TABLE trips ADD COLUMN sortingIndex INTEGER DEFAULT 1;
-        UPDATE trips SET sortingIndex = 1 WHERE sortingIndex IS NULL;
-    END IF;
-END $$;
+-- DO $$ 
+-- BEGIN
+--     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='trips' AND column_name='sortingIndex') THEN
+--         ALTER TABLE trips ADD COLUMN sortingIndex INTEGER DEFAULT 1;
+--         UPDATE trips SET sortingIndex = 1 WHERE sortingIndex IS NULL;
+--     END IF;
+-- END $$;
